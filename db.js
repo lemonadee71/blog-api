36444ed245fd40aaf4db82f62024b0418ae676f7
Shin Andrei Riego <riegoshinandrei@gmail.com>
2023-06-26 14:40:10 +0800

    Add database

---
+++ db.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies, no-console  */
+const mongoose = require('mongoose');
+
+exports.connectDB = async function () {
+  mongoose.Promise = global.Promise;
+
+  let uri;
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    uri = process.env.MONGODB_URI;
+  } else {
+    const { MongoMemoryServer } = require('mongodb-memory-server');
+    const memoryServer = await MongoMemoryServer.create();
+    uri = memoryServer.getUri();
+  }
+
+  // Set up mongoose connection
+  mongoose.connect(uri, options);
+
+  const db = mongoose.connection;
+
+  db.on('error', (e) => {
+    console.log(e);
+
+    if (e.message.code === 'ETIMEDOUT') {
+      mongoose.connect(uri, options);
+    }
+  });
+
+  db.once('open', () => {
+    console.log(`MongoDB successfully connected to ${uri}`);
+    // if (process.env.NODE_ENV !== 'production') {
+    //   console.log('Populating db...');
+    //   require('./scripts/populateDB')();
+    // }
+  });
+
+  return db;
+};
